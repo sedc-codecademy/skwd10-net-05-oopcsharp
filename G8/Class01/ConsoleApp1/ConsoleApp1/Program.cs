baad904a7ef54e4c99a8e775c2f997fb3f66f29e
petreArsovski <email@example.comarso.pet@outlook.com>
2022-03-08 20:33:59 +0100

    class 01

---
+++ G8/Class01/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            // prints in console and leaves a line after the text
+            Console.WriteLine("Hello G8! We are learning c#!");
+            // prints in console and does NOT leave a line after text
+            Console.Write("Petre ");
+            Console.Write("Angel");
+
+            // waits for user input - does not close the console
+            Console.ReadLine();
+        }
+    }
+}
