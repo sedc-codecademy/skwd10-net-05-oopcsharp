7dd75e5f41c07318112fb2d82116d67d566cd6e9
tstojanovska <stojanovskatanja03@gmail.com>
2022-03-09 21:04:17 +0100

    Class 02

---
+++ G4/Class02/Code/Class02/Exercise02/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Exercise02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstString = "Hello";
+            string secondString = "World";
+
+            string concatenationResult = firstString + " " + secondString;
+            Console.WriteLine(concatenationResult);
+
+            string interpolationResult = $"{firstString} {secondString}";
+            Console.WriteLine(interpolationResult);
+
+            string firstStringNumber = "9";
+            string secondStringNumber = "3";
+
+            string stringNumbersConcatenation = firstStringNumber + secondStringNumber;
+            Console.WriteLine(stringNumbersConcatenation);
+        }
+    }
+}
