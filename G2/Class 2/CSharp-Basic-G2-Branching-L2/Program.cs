eb768a5adeb2027f4532fc2fb15c125492827aa3
Mihajlo Dimovski <mihajlo.d96@hotmail.com>
2022-03-10 21:44:09 +0100

    added material for lesson 2

---
+++ G2/Class 2/CSharp-Basic-G2-Branching-L2/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace CSharp_Basic_G2_Branching_L2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vnesete dva brooja");
+            string firstNumberString = Console.ReadLine();
+            string secondNumberString = Console.ReadLine();
+
+            int firstValue = int.Parse(firstNumberString);
+            int secondValue = int.Parse(secondNumberString);
+            
+            // Check if the second value is greater than the first value
+            if (secondValue > firstValue)
+            {
+                Console.WriteLine($"{secondValue} e pogolem od {firstValue}");
+            }
+            // If the case above is not satisfied, check if the first value is greater
+            // than the second
+            else if (firstValue > secondValue)
+            {
+                Console.WriteLine($"{firstValue} e pogolem od {secondValue}");
+            }
+            // If both cases are not satisfied, execute the else block
+            else
+            {
+                Console.WriteLine("Dvata broja se ednakvi");
+            }
+
+            string input;
+            input = Console.ReadLine();
+
+            // Switch statement based on the given input
+            switch (input)
+            {
+                // If the value is equal to "a" or "b", print the line below
+                case "a":
+                case "b":
+                {
+                    Console.WriteLine("Vnesena e bukvata a ili b");
+                }
+                // Exit from the switch
+                break;
+                // If the value is equal to "c", print the line below
+                case "c":
+                {
+                    Console.WriteLine("Vnesena e bukvata c");
+                }
+                break;
+                // If none of the cases above are satisfied, execute the default block
+                default:
+                {
+                    Console.WriteLine("Default block");
+                }
+                break;
+            }
+        }
+    }
+}
