4567d4fbaaf4991f46acee52f4d01b4a1074965e
rpanchevski <risto.panchevski@gmail.com>
2022-03-14 21:08:17 +0100

    Class 03

---
+++ G1/Class03/ArraysExample/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace ArraysExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[2];
+            //numbers = new int[2];
+            numbers[0] = 5;
+            numbers[1] = 7;
+
+            int[] numbersArray = new int[2] { 2, -10};
+
+            float[] koeficienti = new float[] {1.5F, 2.50F, 3.2F, 1.7F, 4};
+
+            Console.WriteLine(koeficienti[2]);
+
+            string[] names = new string[5] {"Risto", "Dejan", "Test", "Dejan", "R"};
+            Console.WriteLine(names[0]);
+            int index = Array.LastIndexOf(names, "Darko");
+            string lastElement = names[names.Length - 1];
+            Console.WriteLine(index);
+            //Console.WriteLine(names[5]); - throws exception
+
+            Console.WriteLine(names.Length);
+            for(int i = 0; i < names.Length; i++)
+            {
+                Console.WriteLine(names[i]);
+            }
+
+            Array.Reverse(names);
+
+            string[] cars = new string[] { "Toyota", "Renault", "Peugeot" };
+            string[] carsCopy = new string[3];
+            cars.CopyTo(carsCopy, 0);
+
+            cars[0] = "bmw";
+            int increase = 5;
+
+            Array.Resize(ref cars, cars.Length + increase);
+            
+            string s = cars[3];
+
+            foreach(string car in cars)
+            {
+                Console.WriteLine(car);
+            }
+        }
+    }
+}
---
+++ G1/Class03/Exercise3/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Exercise3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            int counter = 0;
+
+            while (true)
+            {
+                Console.WriteLine("Ve molime vnesete broj:");
+                string input = Console.ReadLine();
+
+                if (!int.TryParse(input, out number))
+                {
+                    //nevaliden broj
+                    Console.WriteLine("Vnesovte vrednost koja ne e broj!");
+                    continue;
+                }
+
+                break;
+            }
+
+            for (int i = 1; i <= number; i++)
+            {
+                if (i % 3 == 0 || i % 7 == 0)
+                {
+                    continue;
+                }
+
+                counter++;
+
+                if (counter > 100)
+                {
+                    Console.WriteLine("Limit reached");
+                    break;
+                }
+
+                Console.WriteLine("Poraka br: " + counter + " Vrednost: " + i);
+            }
+        }
+    }
+}
