aeb424e52fea3b1dbcf118cca95e3bb742c4e91b
Trajan Stevkovski <trajan.stevkovski@seavus.com>
2022-03-16 18:31:17 +0100

    Class05 part-01

---
+++ G3/Class05/SEDC.Oop.Class05/SEDC.Oop.Class05.Classes/Models/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SEDC.Oop.Class05.Classes.Models
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        private long SSN { get; set; }
+
+        private long GenerateSSN()
+        {
+            Random rand = new Random();
+            long ssn = rand.Next(100000, 999999);
+            return ssn;
+        }
+
+        public void Talk(string text)
+        {
+            Console.WriteLine($"The human named {Name}(SSN:{SSN}) is saying: {text}");
+        }
+    }
+}
---
+++ G3/Class05/SEDC.Oop.Class05/SEDC.Oop.Class05.Classes/Program.cs
@@ -0,0 +1,27 @@
+﻿using SEDC.Oop.Class05.Classes.Models;
+using System;
+
+namespace SEDC.Oop.Class05.Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.Age = 33;
+            person.Name = "Trajan";
+
+            person.Talk("Hello World!");
+
+            Person person1 = new Person()
+            {
+                Age = 22,
+                Name = "Vlatko"
+            };
+
+            person1.Talk("Hello Students!");
+
+            Console.ReadLine();
+        }
+    }
+}
