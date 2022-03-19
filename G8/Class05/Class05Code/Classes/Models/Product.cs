f2ca23d3081309bcb8ead719f98f90a928560b27
petreArsovski <email@example.comarso.pet@outlook.com>
2022-03-19 12:25:11 +0100

    Class 05

---
+++ G8/Class05/Class05Code/Classes/Models/Product.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes.Models
+{
+    public class Product
+    {
+        public int Code { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        //public Product()
+        //{
+
+        //}
+
+        public void Buy(double amount)
+        {
+            if(amount >= Price)
+            {
+                Console.WriteLine($"You can buy the product: {Name}");
+            }
+            else
+            {
+                Console.WriteLine($"You can not buy the product");
+            }
+        }
+    }
+}
---
+++ G8/Class05/Class05Code/Exercise2/Models/Dog.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercise2.Models
+{
+    public class Dog
+    {
+        private string _name { get; set; }
+        public string Race { get; set; }
+        public string Color { get; set; }
+
+        //extra method
+        public void SetName(string name)
+        {
+            if (String.IsNullOrEmpty(name))
+            {
+                throw new Exception("Parameter null has no value");
+            }
+            _name = name;
+        }
+
+        public string Play()
+        {
+            return $"The dog {_name}, from race {Race} is playing";
+        }
+
+        public string Eat(string food)
+        {
+            return $"The dog {_name}, from race {Race} is eating {food}";
+        }
+
+        public string ChaseTail()
+        {
+            return $"The dog {_name}, from race {Race}, with color {Color} is chasing tail";
+        }
+    }
+}
