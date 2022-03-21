90db814b61709083821c4a28e788da104a67ea48
tstojanovska <stojanovskatanja03@gmail.com>
2022-03-21 21:00:50 +0100

    Class06

---
+++ G4/Class06/Code/RetroExercise/Models/Student.cs
@@ -0,0 +1,32 @@
+﻿
+
+using System;
+
+namespace RetroExercise.Models
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public string Academy { get; set; }
+        public string Group { get; set; }
+
+        public Student(string name, string academy, string group)
+        {
+            if (String.IsNullOrEmpty(name))
+            {
+                throw new Exception("Name must not be empty!");
+            }
+            if (String.IsNullOrEmpty(academy))
+            {
+                throw new Exception("Academy must not be empty!");
+            }
+            if (String.IsNullOrEmpty(group))
+            {
+                throw new Exception("Group must not be empty!");
+            }
+            Name = name;
+            Academy = academy;
+            Group = group;
+        }
+    }
+}
