13234ce93e05d429c81a2473705ba41f1f658127
Trajan Stevkovski <trajan.stevkovski@seavus.com>
2022-03-23 18:30:00 +0100

    Class07

---
+++ G3/Class07/SEDC.Oop.Class07/SEDC.Oop.Class07.Inheritance/Models/Cat.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SEDC.Oop.Class07.Inheritance.Models
+{
+    public class Cat : Animal
+    {
+        public bool IsLazy { get; set; }
+
+        public Cat()
+        {
+            Console.WriteLine("A new instance of cat is created by the parent constructor.");
+        }
+
+        public Cat(string name, string type, string race, bool isLazy)
+            : base(name, type, race)
+        {
+            IsLazy = isLazy;
+            Console.WriteLine("A new instance of cat is created by the parent constructor with params.");
+        }
+
+        public override void Eat()
+        {
+            if(IsLazy)
+            {
+                Console.WriteLine("We need to feed teh cat with a spoon!");
+            }
+            else
+            {
+                Console.WriteLine("The cat is eating the food!");
+            }
+        }
+    }
+}
