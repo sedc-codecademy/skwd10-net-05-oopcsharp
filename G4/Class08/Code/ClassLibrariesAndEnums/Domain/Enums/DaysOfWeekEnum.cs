2e3b318e182514d7aab80b2f55850cf409bdd900
tstojanovska <stojanovskatanja03@gmail.com>
2022-03-26 12:39:04 +0100

    Class 08

---
+++ G4/Class08/Code/ClassLibrariesAndEnums/Domain/Enums/DaysOfWeekEnum.cs
@@ -0,0 +1,15 @@
+﻿
+
+namespace Domain.Enums
+{
+    public enum DaysOfWeekEnum
+    {
+        Monday = 1,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
+    }
+}
---
+++ G4/Class08/Code/Exercise/Domain/Models/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Domain.Enums;
+
+namespace Domain.Models
+{
+    public class Employee
+    {
+        protected double Salary { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public RoleEnum Role { get; set; }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine($"{FirstName} {LastName} - {Salary}$.");
+        }
+
+        public virtual double GetSalary()
+        {
+            return Salary;
+        }
+    }
+}
