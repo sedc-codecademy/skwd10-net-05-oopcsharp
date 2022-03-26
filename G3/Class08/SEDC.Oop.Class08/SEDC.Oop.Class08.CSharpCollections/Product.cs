5cfd1d811af10a6a013d65b537c8adb0fb5b0a11
Trajan Stevkovski <trajan.stevkovski@seavus.com>
2022-03-26 12:29:27 +0100

    Class08

---
+++ G3/Class08/SEDC.Oop.Class08/SEDC.Oop.Class08.CSharpCollections/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SEDC.Oop.Class08.CSharpCollections
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public Category Category { get; set; }
+    }
+
+    public enum Category
+    {
+        ItEquipment,
+        LapTop,
+        PC,
+        TV
+    }
+}
--- G3/Class08/SEDC.Oop.Class08/SEDC.Oop.Class08.CSharpCollections/Program.cs
+++ G3/Class08/SEDC.Oop.Class08/SEDC.Oop.Class08.CSharpCollections/Program.cs
@@ -95,6 +95,32 @@ namespace SEDC.Oop.Class08.CSharpCollections
             GoThruCollection(someStack, "STACK");
             #endregion
 
+            #region Exercise
+            List<Product> products = new List<Product>
+            {
+                new Product() { Name =  "Keyboard", Price = 100, Category = Category.ItEquipment},
+                new Product() { Name =  "Mouse", Price = 150, Category = Category.ItEquipment},
+                new Product() { Name =  "Headphones", Price = 80, Category = Category.ItEquipment},
+                new Product() { Name =  "Microphone", Price = 220, Category = Category.ItEquipment},
+                new Product() { Name =  "Camera", Price = 1000, Category = Category.ItEquipment},
+                new Product() { Name =  "Hp-580", Price = 20000, Category = Category.LapTop},
+                new Product() { Name =  "Lenovo G200", Price = 18500, Category = Category.LapTop},
+                new Product() { Name =  "Hp ProBook", Price = 12000, Category = Category.LapTop},
+                new Product() { Name =  "Acer S1200", Price = 28000, Category = Category.LapTop},
+                new Product() { Name =  "Dell MS300", Price = 74000, Category = Category.LapTop},
+                new Product() { Name =  "Office 1", Price = 10200, Category = Category.PC},
+                new Product() { Name =  "Office 2", Price = 12400, Category = Category.PC},
+                new Product() { Name =  "Gaming", Price = 22000, Category = Category.PC},
+                new Product() { Name =  "Ultra Gaming", Price = 32200, Category = Category.PC},
+                new Product() { Name =  "Ultra Ultra Gaming", Price = 61600, Category = Category.PC},
+                new Product() { Name =  "Alienware", Price = 128000, Category = Category.PC},
+                new Product() { Name =  "Samsung Proview", Price = 56200, Category = Category.TV},
+                new Product() { Name =  "LG LG420MQ8", Price = 32000, Category = Category.TV},
+                new Product() { Name =  "SONY Plasma", Price = 18000, Category = Category.TV},
+                new Product() { Name =  "SONY RW78OS", Price = 179999, Category = Category.TV},
+                new Product() { Name =  "Samsung Oval", Price = 247999, Category = Category.TV}
+            };
+            #endregion
 
             Console.ReadLine();
         }
@@ -107,6 +133,19 @@ namespace SEDC.Oop.Class08.CSharpCollections
                 Console.WriteLine("{0}", item);
             }
             Console.WriteLine();
+        }
+
+        public static List<Product> SearchByCategory(List<Product> products, Category category)
+        {
+            List<Product> listOfProducts = new List<Product>();
+            foreach(Product product in products)
+            {
+                if(product.Category == category)
+                {
+                    listOfProducts.Add(product);
+                }
+            }
+            return listOfProducts;
         }
     }
 
