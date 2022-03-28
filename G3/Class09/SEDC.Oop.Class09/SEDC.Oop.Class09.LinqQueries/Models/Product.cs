728c85e881879fb4c8bda4290aee49bb5fa6d90a
Trajan Stevkovski <trajan.stevkovski@seavus.com>
2022-03-28 18:43:49 +0200

    Class09

---
+++ G3/Class09/SEDC.Oop.Class09/SEDC.Oop.Class09.LinqQueries/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SEDC.Oop.Class09.LinqQueries.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public Category Category { get; set; }
+    }
+}
